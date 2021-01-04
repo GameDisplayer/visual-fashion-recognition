e21ee79f8012bc95ade4b6c938741a38fa767204
Romain <romainmichelucci@protonmail.com>
2021-01-04 16:06:38 +0100

    TL experiment
    #12
    ResNet101 with some data augmentation but too much images and too long ! (failed at creating fashion subset)

---
+++ Tests_func/transferLearning.m
@@ -0,0 +1,93 @@
+%% Load Data
+train_folder = "C:\Users\Romain\Desktop\Visual Information\PROJECT\Data\train\train";
+T = readtable("C:\Users\Romain\Desktop\Visual Information\PROJECT\Data\train.csv");
+
+%% Sort images by natural order 
+S = dir(fullfile(train_folder,'*.jpg'));
+N = natsortfiles({S.name});
+F = cellfun(@(n)fullfile(train_folder,n),N,'uni',0);
+I = imageDatastore(string(F),'Labels', categorical(T.class));
+
+%% Subset for fashion images
+% sub = T.class < 3700; %39762
+% indices = 1:sum(sub);
+% imds = subset(I, indices);
+imds = I
+
+%% Split data
+[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7);
+
+%% Load pretrained network
+net = resnet101;
+inputSize = net.Layers(1).InputSize;
+
+%% Replace Final layers
+if isa(net,'SeriesNetwork') 
+  lgraph = layerGraph(net.Layers); 
+else
+  lgraph = layerGraph(net);
+end 
+
+[learnableLayer,classLayer] = findLayersToReplace(lgraph);
+[learnableLayer,classLayer] 
+
+numClasses = 9691; %3700
+
+
+if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
+    newLearnableLayer = fullyConnectedLayer(numClasses, ...
+        'Name','new_fc', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+    
+elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
+    newLearnableLayer = convolution2dLayer(1,numClasses, ...
+        'Name','new_conv', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+end
+
+lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
+
+newClassLayer = classificationLayer('Name','new_classoutput');
+lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
+
+figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
+plot(lgraph)
+ylim([0,10])
+
+%% Freeze initial layers
+
+layers = lgraph.Layers;
+connections = lgraph.Connections;
+
+layers(1:10) = freezeWeights(layers(1:10));
+lgraph = createLgraphUsingConnections(layers,connections);
+
+%% Train Network
+pixelRange = [-30 30];
+scaleRange = [0.9 1.1];
+imageAugmenter = imageDataAugmenter( ...
+    'RandXReflection',true, ...
+    'RandXTranslation',pixelRange, ...
+    'RandYTranslation',pixelRange, ...
+    'RandXScale',scaleRange, ...
+    'RandYScale',scaleRange);
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
+    'DataAugmentation',imageAugmenter);
+
+augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
+
+miniBatchSize = 10;
+valFrequency = floor(numel(augimdsTrain.Files)/miniBatchSize);
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',miniBatchSize, ...
+    'MaxEpochs',6, ...
+    'InitialLearnRate',3e-4, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',augimdsValidation, ...
+    'ValidationFrequency',valFrequency, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+net = trainNetwork(augimdsTrain,lgraph,options);
