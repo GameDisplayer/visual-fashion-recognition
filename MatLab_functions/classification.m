19f596156c612b5be7ff8ab88574a3fb7e1937e0
Romain <romainmichelucci@protonmail.com>
2021-02-15 16:03:13 +0100

    Resnet101 TL classification & integration
    #12

---
+++ MatLab_functions/classification.m
@@ -0,0 +1,20 @@
+%% Classification of image sent
+function classification(image)
+
+ %Load the pre-trained network
+    load('resnet101.mat');
+
+    %Resize image for classification
+    I = imread(image);
+    inputSize=net.Layers(1).InputSize;
+    I=imresize(I, inputSize(1:2));
+
+    %Classify the Image
+    label = classify(net, I);
+    message = "The label predicted is " + char(label(1));
+
+    fileID = fopen('label.txt','w');
+    fprintf(fileID,message);
+    fclose(fileID);
+end
+   
